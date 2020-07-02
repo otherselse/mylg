cef18692d493b7c098c7164545925bc2bf40a8e8
xyy <otherselse@163.com>
2020-07-02 20:09:01 +0800

    start

--- js/mylg.js
+++ js/mylg.js
@@ -13,5 +13,14 @@ $(function(){
 //set left height
 function setHeight(){
 	var h=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;   //屏幕的高度
-	$(".menu").height(h-140);
+	$(".menu").height(h-143);
+}
+
+
+
+//左侧菜单展开收起
+function showsMenu(obj){
+	var myobj=$(obj).find(".smenu");
+	myobj.slideToggle();
+	$(obj).toggleClass("open")
 }
---
+++ js/topBar.js
@@ -0,0 +1,35 @@
+(function(){
+	var str1=
+		'<div class="topBar">'
+		+'	<div class="pl30 pr30 pt20 ovh">'
+		+'		<span class="l pct20 mt5"><i class="id_mylg lgIcon vm mr5"></i><span class="g28 dib f20 vm">我的乐刚</span></span>'
+		+'		<div class="l pct60 tc">'
+		+'			<span class="ui-switchCont">'
+		+'				<a href="index.html" class="ui-switchBut on">我是买家</a><a href="buy.html" class="ui-switchBut">我是卖家</a>'
+		+'			</span>'
+		+'		</div>'
+		+'		<div class="r pct20 tr">'
+		+'			<a class="lgIcon topcart vm mr20" href="##"><i class="topcartDot">3</i></a>'
+		+'			<img src="../images/exp.png" class="topImg vm mr5"><a href="javascript:void(0)" class="tipclick" title="<a href=\'exit\' class=\'pr10 pl10 g67\'>退出</a>">hi,钢铁大王</a>'
+		+'		</div>'
+		+'	</div>'
+		'</div>'
+
+	document.write(str1)
+})();
+
+
+$('.tipclick').qtip({
+	show: { event: 'click' },
+	hide: 'unfocus',
+	position: {
+		viewport: $(window)
+	},
+	style: {
+		classes: 'qtip-lg qtip-light  qtip-shadow qtip-rounded'  //
+	}
+});
+
+
+
+
