0ecb550db00824f2fa32f077e0fa2edbf79a07a1
xyy <otherselse@163.com>
2020-07-10 20:07:14 +0800

    account

--- js/mylg.js
+++ js/mylg.js
@@ -115,3 +115,14 @@ function setAll(obj) {
 		content:'../supplier/wareHouse.html'
 	});
  }
+
+
+ 
+//阻止冒泡
+function stopPro(e){
+	if(navigator.appName == "Microsoft Internet Explorer" && (navigator.appVersion.match(/7./i)=="7."|| navigator.appVersion.match(/8./i)=="8.")) 
+	{
+		if(event.stopPropagation) { event.stopPropagation();}
+		else if(window.event){ window.event.cancelBubble = true;}
+	}else{ e.stopPropagation(); }	
+}
